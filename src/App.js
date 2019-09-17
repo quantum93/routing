eddde27a774436437775610972044304333c22db
Student <example@example.com>
2019-09-17 14:21:04 -0700

    It adds Router

--- src/App.js
+++ src/App.js
@@ -3,14 +3,17 @@ import Nav from './Nav';
 import About from './about';
 import Shop from './shop';
 import './App.css';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 function App() {
   return (
+    <Router>
     <div className="App">
       <Nav />
-      <About />
-      <Shop />
+      <Route path="/about" component={About}/>
+      <Route path="/shop" component={Shop}/>
     </div>
+  </Router>
   );
 }
 
